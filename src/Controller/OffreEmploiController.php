73faefbb0550023bfa2c6c64f8a4b38c6e87583c
Alexandre-le-Grand <a.thomas@lprs.fr>
2023-10-17 12:29:06 +0200

    Creation d'offre d'emploi

--- src/Controller/OffreEmploiController.php
+++ src/Controller/OffreEmploiController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Amphitheatre;
+use App\Entity\OffreEmploi;
+use App\Form\AmphitheatreType;
+use App\Form\OffreEmploiType;
 use App\Repository\OffreEmploiRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,6 +31,30 @@ class OffreEmploiController extends AbstractController
         );
         return $this->render('pages/offre_emploi/index.html.twig', [
             'offres' => $offre
+        ]);
+    }
+    #[Route('/offre_emploi/nouveau', 'offreemploi.new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_REPRESENTANT')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $offreEmploi = new OffreEmploi();
+        $form = $this->createForm(OffreEmploiType::class, $offreEmploi);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($offreEmploi);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'offre d'emploi a été créée avec succès !"
+            );
+
+            return $this->redirectToRoute('offreemploi_index');
+        }
+
+        return $this->render('pages/offre_emploi/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/OffreEmploiType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\OffreEmploi;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OffreEmploiType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'attr' =>  [
+                    'class'=> 'form-control'
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'attr' =>  [
+                    'class'=> 'form-control'
+                ],
+            ])
+            ->add('type_contrat', ChoiceType::class, [
+                'attr' =>  [
+                    'class'=> 'form-control'
+                ],
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Créer l\'offre d\'emploi'
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => OffreEmploi::class,
+        ]);
+    }
+}
