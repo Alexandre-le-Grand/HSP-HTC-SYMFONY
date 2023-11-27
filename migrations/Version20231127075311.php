31a03ba1298b7c708b69d073d18c8854808506aa
ZINS_Hu <h.zins@lprs.fr>
2023-11-27 08:54:53 +0100

    Correction erreur reset_token modèle base de données

--- migrations/Version20231127075311.php
+++ migrations/Version20231127075311.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231120184538 extends AbstractMigration
+final class Version20231127075311 extends AbstractMigration
 {
     public function getDescription(): string
     {
